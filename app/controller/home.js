80e93d4d896362959106db67cef9e74e02e764f1
liuweinefu <liuweinefu@qq.com>
2018-08-18 14:57:00 +0800

    20180818

--- app/controller/home.js
+++ app/controller/home.js
@@ -3,33 +3,36 @@
 const Controller = require('egg').Controller;
 
 class HomeController extends Controller {
-  async index() {
-    if (this.ctx.session.count == undefined) { this.ctx.session.count = 1 }
-    if (this.ctx.session.count < 100) {
-      this.ctx.session.count++;
+    async index() {
+        const ctx = this.ctx;
+        const dataList = {};
+        await ctx.render('index.tpl', dataList);
     }
-    var text = this.ctx.session.count;
-    this.ctx.body = 'hi, egg' + await this.ctx.service.one.printOne(text) + await this.ctx.service.one.printTwo(text) + await this.ctx.service.one.printThree(text);
-  }
-  async list() {
-    const ctx = this.ctx;
-    //ctx.redirect(`http://job.nefu.edu.cn/`);
-    //this.ctx.body = 'hi, user';
-    const dataList = {};
-    await ctx.render('home.tpl', dataList);
-  }
-  async getMenu() {
-    console.log('here');
-    const ctx = this.ctx;
-    //ctx.redirect(`http://job.nefu.edu.cn/`);
-    //this.ctx.body = 'hi, user';
-    // const dataList = {};
-    // await ctx.render('home.tpl', dataList);
-    const menu = [{ sn: 1, name: '首页', router: '/' }, { sn: 2, name: '列表', router: '/list' }];
-    ctx.body = {
-      menus: menu,
-    };
-  }
+
+    async list() {
+        const ctx = this.ctx;
+        //ctx.redirect(`http://job.nefu.edu.cn/`);
+        //this.ctx.body = 'hi, user';
+        const dataList = {};
+        await ctx.render('index.tpl', dataList);
+    }
+    async getMenu() {
+        console.log('here');
+
+        //ctx.redirect(`http://job.nefu.edu.cn/`);
+        //this.ctx.body = 'hi, user';
+        // const dataList = {};
+        // await ctx.render('home.tpl', dataList);
+
+        // const menu = [{ sn: 1, name: '首页', router: '/' }, { sn: 2, name: '列表', router: '/list' }];
+        const { ctx } = this;
+        //const S = ctx.service;
+        const M = ctx.model;
+        let menu = await M.Menu.findAll();
+        ctx.body = {
+            menus: menu,
+        };
+    }
 }
 
 module.exports = HomeController;
--- app/router.js
+++ app/router.js
@@ -9,13 +9,25 @@ module.exports = app => {
   // router.get('/', controller.home.index);
   // router.get('/list', controller.home.list);
   // router.post('/getMenu', controller.home.getMenu);
-  router.get('/index', controller.index.index);
+  router.get('/', controller.home.index);
+  router.post('/getMenu', controller.home.getMenu);
+
+  const tableControllers = ['user', 'userType'];
+
+  tableControllers.forEach(ctr => {
+    router.get('/' + ctr, controller[ctr].getTpl);
+    router.post('/' + ctr + '/findAll', controller[ctr].findAll);
+    router.post('/' + ctr + '/save', controller[ctr].save);
+    router.post('/' + ctr + '/update', controller[ctr].replace);
+  })
 
 
-  router.get('/user', controller.user.getTpl);
-  router.post('/user/findAll', controller.user.findAll);
-  router.post('/user/save', controller.user.save);
-  router.post('/user/update', controller.user.replace);
-  router.post('/userType/findAll', controller.userType.findAll);
+
+  // router.get('/user', controller.user.getTpl);
+  // router.post('/user/findAll', controller.user.findAll);
+  // router.post('/user/save', controller.user.save);
+  // router.post('/user/update', controller.user.replace);
+
+  // router.post('/userType/findAll', controller.userType.findAll);
   //router.get('/two', controller.two.getTpl);
 };
